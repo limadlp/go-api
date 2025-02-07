4e86395a7a83ed9fb6b3800d192454e0416e1c99
limadlp <dlplima@hotmail.com>
2025-02-06 21:56:42 -0300

    feat: add layers

---
+++ controller/product_controller.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"go-api/model"
+	"go-api/usecase"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type productController struct {
+	productUseCase usecase.ProductUsecase
+}
+
+func NewProductController(usecase usecase.ProductUsecase) productController {
+	return productController{
+		productUseCase: usecase,
+	}
+}
+
+func (p *productController) GetProducts(ctx *gin.Context) {
+	products := []model.Product{
+
+		{
+			ID:    1,
+			Name:  "Product 1",
+			Price: 1000,
+		},
+	}
+
+	ctx.JSON(http.StatusOK, products)
+}
---
+++ usecase/product_usecase.go
@@ -0,0 +1,20 @@
+package usecase
+
+import (
+	"go-api/model"
+	"go-api/repository"
+)
+
+type ProductUsecase struct {
+	repository repository.ProductRepository
+}
+
+func NewProductUsecase(repo repository.ProductRepository) ProductUsecase {
+	return ProductUsecase{
+		repository: repo,
+	}
+}
+
+func (pu *ProductUsecase) GetProducts() ([]model.Product, error) {
+	return []model.Product{}, nil
+}
